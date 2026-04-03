e05585e5b050e9691397f6e69ac265c33fe84521
lsbraga <lsbragadev@gmail.com>
2026-04-03 10:55:35 -0300

    Update Maior_numero.py

--- Aula 01/Maior_numero.py
+++ Aula 01/Maior_numero.py
@@ -1,12 +1,13 @@
 #Maior Número
 
+#Inicio
 print("DESCUBRA QUAL É O MAIOR NÚMERO")
-
+#Entrada
 maior = float(input("Digite o primeiro númeor:\n"))
-
+#Ação
 for i in range(2, 6):
     numero = float(input("Digite o {}º número: ".format(i)))
     if numero > maior:
         maior = numero
-
+#Saída
 print("O maior número é:", maior)
